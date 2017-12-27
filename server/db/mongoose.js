1780067ea1cea9b2216497ace64ec367dc448eeb
Lasagna Larry <docroflcopter@gmail.com>
2017-12-27 18:24:36 -0500

    server db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://default:default@ds163806.mlab.com:63806/nodejs-todos', {
-//     useMongoClient: true
-// });
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect('mongodb://default:default@ds163806.mlab.com:63806/nodejs-todos', {
     useMongoClient: true
 });
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {
+//     useMongoClient: true
+// });
 module.exports = {
     mongoose
 }
