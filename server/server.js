6c162019a5bd717a02ef49b0190dd96f119f7802
Lasagna Larry <docroflcopter@gmail.com>
2018-01-06 15:33:37 -0500

    Fix 2.0

--- server/server.js
+++ server/server.js
@@ -10,8 +10,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 const cors = require('cors');
-const history = require('connect-history-api-fallback');
-const connect = require('connect');
 
 const port = process.env.PORT || 3000
 
@@ -21,6 +19,16 @@ app.use(bodyParser.json());
 app.use(cors({
   exposedHeaders: ['x-auth', 'X-Auth', 'Content-Type']
 }));
+
+app.get('/', (request, response) => {
+  response.render(`${__dirname}/public/index.html`)
+});
+
+app.get('/test', (req, res) => {
+  res.send({
+    test: 'Completed'
+  });
+});
 
 // Add new todo and return it
 app.post('/todos', authenticate, (req, res) => {
