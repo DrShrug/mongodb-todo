3bdc562df06a33b61439185a9ec196c3b80847d2
Lasagna Larry <docroflcopter@gmail.com>
2018-01-11 18:21:58 -0500

    at all

--- server/server.js
+++ server/server.js
@@ -163,12 +163,12 @@ app.get('/users/me', authenticate, (req, res) => {
   Todo.find({
     _creator: req.user._id
   }).then((todos) => {
+    console.log(todos);
     userToJson.todos = todos.toObject();
-    res.send(userToJson);
   }, (err) => {
     res.status(400).send(e);
   });
-  
+  res.send(userToJson);
 });
 
 app.listen(port, () => {
