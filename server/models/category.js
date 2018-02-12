954999564ef3f95e15c2eb1864c7211fc17987b2
Lasagna Larry <docroflcopter@gmail.com>
2018-02-11 19:41:46 -0500

    Implemented cascade delete
    + Deleting a group now actually deletes the categories and todos associated with it

--- server/models/category.js
+++ server/models/category.js
@@ -2,29 +2,36 @@ const mongoose = require('mongoose');
 const { Todo } = require('./todo');
 
 var categorySchema = mongoose.Schema({
-    categoryName: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    _creator: {
-        required: true,
-        type: mongoose.Schema.Types.ObjectId
-    },
-    _group: {
-        required: true,
-        type: mongoose.Schema.Types.ObjectId
-    }
+  categoryName: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  _creator: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
+  },
+  _group: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
+  }
 });
 
+// Called by DELETE Category route
 categorySchema.pre('findOneAndRemove', function (next) {
-    Todo.deleteMany({ _category: this._conditions._id._id });
-    next();
+  Todo.remove({ _category: this._conditions._id._id}).exec();
+  next();
 });
+
+// Called by Group DELETE route middleware
+categorySchema.post('remove', (doc) => {
+  Todo.remove({ _category: doc._id }).exec();
+});
+
 
 var Category = mongoose.model('Category', categorySchema);
 
 module.exports = {
-    Category
+  Category
 }
--- server/models/group.js
+++ server/models/group.js
@@ -1,32 +1,42 @@
 const mongoose = require('mongoose');
+const { Category } = require('./category');
 
 var groupSchema = mongoose.Schema({
   groupName: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 1,
-        maxlength: 30,
-        trim: true
-    },
-    _owner: {
-      required: true,
-      type: mongoose.Schema.Types.ObjectId
-    },
-    members: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    categories: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-    }]
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 1,
+    maxlength: 30,
+    trim: true
+  },
+  _owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  }]
 });
 
 groupSchema.index({ 'members': 1 });
 
+groupSchema.pre('findOneAndRemove', function (next) {
+  Category.find({ _group: this._conditions._id._id }).then((categories) => {
+    categories.forEach((category) => {
+      category.remove();
+    });
+  });
+  next();
+});
+
 var Group = mongoose.model('Group', groupSchema);
 
 module.exports = {
-    Group
+  Group
 }
