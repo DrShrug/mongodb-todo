0117ae0fbff34ece03c54812d90d912d4b917062
PhuLN <docroflcopter@gmail.com>
2018-02-21 17:38:39 -0500

    Testing double push

--- server/app/server.js
+++ server/app/server.js
@@ -8,6 +8,8 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000
 
+var test = 'test';
+
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
